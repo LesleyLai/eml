7a8df1d29cb845f511d31fe175218d1b8d2d7ebd
Lesley <lesleylai@lesleylai.info>
2018-12-01 19:07:22 -0700

    A working web repl

---
+++ src/web/src/Repl.bs.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var List = require("bs-platform/lib/js/list.js");
+var $$Array = require("bs-platform/lib/js/array.js");
+var Block = require("bs-platform/lib/js/block.js");
+var Curry = require("bs-platform/lib/js/curry.js");
+var React = require("react");
+var ReasonReact = require("reason-react/src/ReasonReact.js");
+
+function record_to_elem(record) {
+  return React.createElement("div", undefined, React.createElement("p", undefined, "> " + record[/* command */0]), record[/* response */1].length !== 0 ? React.createElement("p", undefined, "  " + record[/* response */1]) : "");
+}
+
+var component = ReasonReact.reducerComponent("Example");
+
+function make(_children) {
+  return /* record */[
+          /* debugName */component[/* debugName */0],
+          /* reactClassInternal */component[/* reactClassInternal */1],
+          /* handedOffState */component[/* handedOffState */2],
+          /* willReceiveProps */component[/* willReceiveProps */3],
+          /* didMount */component[/* didMount */4],
+          /* didUpdate */component[/* didUpdate */5],
+          /* willUnmount */component[/* willUnmount */6],
+          /* willUpdate */component[/* willUpdate */7],
+          /* shouldUpdate */component[/* shouldUpdate */8],
+          /* render */(function (self) {
+              return React.createElement("div", undefined, $$Array.of_list(List.rev(List.map(record_to_elem, self[/* state */1][/* history */0]))), React.createElement("span", undefined, "> "), React.createElement("input", {
+                              style: {
+                                border: "none",
+                                color: "#444444",
+                                width: "90%"
+                              },
+                              onKeyDown: (function ($$event) {
+                                  var match = $$event.which;
+                                  if (match !== 13) {
+                                    return /* () */0;
+                                  } else {
+                                    return Curry._1(self[/* send */3], /* Enter */0);
+                                  }
+                                }),
+                              onChange: (function ($$event) {
+                                  return Curry._1(self[/* send */3], /* InputText */[$$event.target.value]);
+                                })
+                            }));
+            }),
+          /* initialState */(function (param) {
+              return /* record */[
+                      /* history : [] */0,
+                      /* inputTest */""
+                    ];
+            }),
+          /* retainedProps */component[/* retainedProps */11],
+          /* reducer */(function (action, state) {
+              if (action) {
+                return /* Update */Block.__(0, [/* record */[
+                            /* history */state[/* history */0],
+                            /* inputTest */action[0]
+                          ]]);
+              } else {
+                var jsInterpret = ( 
+          function(s) {
+            return Pointer_stringify(interpret(s));
+          }
+        );
+                var request = state[/* inputTest */1];
+                var response = Curry._1(jsInterpret, request);
+                return /* Update */Block.__(0, [/* record */[
+                            /* history : :: */[
+                              /* record */[
+                                /* command */request,
+                                /* response */response
+                              ],
+                              state[/* history */0]
+                            ],
+                            /* inputTest */""
+                          ]]);
+              }
+            }),
+          /* jsElementWrapped */component[/* jsElementWrapped */13]
+        ];
+}
+
+exports.record_to_elem = record_to_elem;
+exports.component = component;
+exports.make = make;
+/* component Not a pure module */
---
+++ src/web/src/Repl.re
@@ -0,0 +1,83 @@
+type record = {
+  command: string,
+  response: string,
+}
+
+let record_to_elem(record: record) = {
+  <div>
+  <p> {ReasonReact.string("> " ++ record.command)} </p>
+  {if (String.length(record.response) != 0) {
+    <p> {ReasonReact.string("  " ++ record.response)} </p>
+  } else {
+    ReasonReact.string("")
+  } }
+  </div>
+}
+
+type state = {
+  history: list(record),
+  inputTest: string
+};
+
+type action =
+    | Enter
+    | InputText(string)
+
+/* Component template declaration.
+   Needs to be **after** state and action declarations! */
+let component = ReasonReact.reducerComponent("Example");
+
+/* greeting and children are props. `children` isn't used, therefore ignored.
+   We ignore it by prepending it with an underscore */
+let make = (_children) => {
+  /* spread the other default fields of component here and override a few */
+  ...component,
+
+  initialState: () => {history: [], inputTest: ""},
+
+  reducer: (action, state) => {
+    switch (action) {
+      | InputText(newTest) => {
+        ReasonReact.Update({...state, inputTest: newTest})
+      }
+      | Enter => {
+        let jsInterpret: string => string = [%bs.raw {| 
+          function(s) {
+            return Pointer_stringify(interpret(s));
+          }
+        |}];
+        let request = state.inputTest;
+        let response = jsInterpret(request);
+        ReasonReact.Update({inputTest: "",
+                            history: [{command: request, response: response}, ...state.history]});
+      }
+      | _ => ReasonReact.NoUpdate
+    }
+  },
+
+  render: self => {
+    <div>
+    (
+          List.map(record_to_elem, self.state.history)
+          -> List.rev
+          -> Array.of_list
+          -> ReasonReact.array
+    )
+
+      <span>
+          {ReasonReact.string("> ")}
+      </span>
+        
+      <input 
+      style=(ReactDOMRe.Style.make(~color="#444444", ~width="90%", ~border="none", ()))
+      onKeyDown=(event => switch(ReactEvent.Keyboard.which(event)) {
+        | 13 => self.send(Enter)
+        | _ => ()
+      })
+      onChange=(event => {
+        self.send(InputText(ReactDOMRe.domElementToObj(ReactEventRe.Form.target(event))##value));
+      })
+      />
+    </div>;
+    },
+};
